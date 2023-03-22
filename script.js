6000b2b55e005a930bfc626dd56535aea3999ed8
=abaysew <abaysewtekle@gmail.com>
2023-03-22 03:57:06 -0700

    Done with the homepage and setup the popup for sections on the page

--- script.js
+++ script.js
@@ -2,3 +2,30 @@
 particlesJS.load("particles-js", "assets/particlesjs-config.json", function () {
   console.log("callback - particles.js config loaded");
 });
+
+const closeBtn = document.querySelectorAll(".close__btn");
+const menus = document.querySelectorAll(".navlinks li a");
+
+let menuItems = ["about", "specialities", "services", "contacts"];
+
+// get the clicked item name
+menus.forEach((btn) => {
+  btn.addEventListener("click", (e) =>
+    getMatch(e.currentTarget.textContent.toLowerCase())
+  );
+});
+
+function getMatch(value) {
+  menuItems.map((menu) => {
+    if (menu === value) {
+      document.getElementById(value).style.display = "flex";
+    }
+  });
+}
+
+// CLOSE BUTTON
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.currentTarget.parentElement.parentElement.style.display = "none";
+  });
+});
