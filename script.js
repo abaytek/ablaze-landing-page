cb6697092fa7d7bc964184d058a6c23cae8a4a78
=abaysew <abaysewtekle@gmail.com>
2023-03-24 05:48:22 -0700

    Added client side form validation to the contact us form

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const menus = document.querySelectorAll(".navlinks li a");
 const btns = document.querySelectorAll(".btn");
 const about = document.getElementById("about");
 const specialities = document.getElementById("specialities");
+let responseMessage = document.querySelector(".response");
+let form = document.querySelector(".form__container");
 
 let menuItems = ["about", "specialities", "services", "contacts"];
 let buttonsText = ["Who we are", "What We Do"];
@@ -43,4 +45,46 @@ btns.forEach((btn) => {
       specialities.style.transform = `translateY(0)`;
     }
   });
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const fullName = document.forms["form"]["fullname"].value;
+  const email = document.forms["form"]["email"].value;
+  const phone = document.forms["form"]["phone"].value;
+  const message = document.forms["form"]["message"].value;
+
+  if (fullName == "") {
+    responseMessage.textContent = "Name Must not Be empty";
+  } else if (email == "") {
+    responseMessage.textContent = "Email Must not Be empty";
+  } else if (phone == "") {
+    responseMessage.textContent = "Phone Must not Be empty";
+  } else if (message == "") {
+    responseMessage.textContent = "Message Must not Be empty";
+  }
+
+  // functions individual Inputs
+  // const isPasswordSecure = (password) => {
+  //   const re = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+  //   return re.test(password);
+  // };
+  const isBetween = (length, min, max) =>
+    length < min || length > max ? false : true;
+  const isEmailValid = (email) => {
+    const re =
+      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  // validate individual Inputs
+  if (!isEmailValid(email)) {
+    responseMessage.textContent = "Invalid Email";
+  } else if (!isBetween(fullName.length, 5, 10)) {
+    responseMessage.textContent = "Name must be 5 to 10 characters long";
+  }
+
+  // Fetch Request to the email server
+  responseMessage.textContent = "Message Sent Succesfully";
+  responseMessage.style.color = "white";
 });
