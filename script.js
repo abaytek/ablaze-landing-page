23f6f429ce675f0a5ab1d980af8967ba12a863d2
=abaysew <abaysewtekle@gmail.com>
2023-03-24 02:40:48 -0700

    Created alink fo the hero buttons

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ particlesJS.load("particles-js", "assets/particlesjs-config.json", function () {
 
 const closeBtn = document.querySelectorAll(".close__btn");
 const menus = document.querySelectorAll(".navlinks li a");
+const btns = document.querySelectorAll(".btn");
+const about = document.getElementById("about");
+const specialities = document.getElementById("specialities");
 
 let menuItems = ["about", "specialities", "services", "contacts"];
+let buttonsText = ["Who we are", "What We Do"];
 
 // get the clicked item name
 menus.forEach((btn) => {
@@ -27,5 +31,16 @@ function getMatch(value) {
 closeBtn.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.currentTarget.parentElement.parentElement.style.transform = `translateY(-100vh)`;
+  });
+});
+
+// WHAT WE ARE AND WHAT WE DO BUTTONS
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.textContent == buttonsText[0]) {
+      services.style.transform = `translateY(0)`;
+    } else if (btn.textContent == buttonsText[1]) {
+      specialities.style.transform = `translateY(0)`;
+    }
   });
 });
